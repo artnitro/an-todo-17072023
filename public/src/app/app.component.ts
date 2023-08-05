990ebb7b922bfbcbcc901823160183cbb617dca8
artnitro <qp.artnitro@gmail.com>
2023-08-05 17:40:33 +0200

    feat: Comenzar proyecto an-todo.
    Asigno etiqueta HTML del proyecto y aviso que está cargando.

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'an-todo',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })

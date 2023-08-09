12e00f4bd5259d8ffa2f0f83a0c1b8057ee95569
artnitro <qp.artnitro@gmail.com>
2023-08-09 12:35:44 +0200

    feat: Modificar etiqueta del proyecto.
    Modifico la etiqueta general del proyecto.

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'an-todo',
+  selector: 'an-todo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
 }

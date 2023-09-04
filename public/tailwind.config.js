2028d65e809e806da76ff404cd17ce134c4e2913
artnitro <qp.artnitro@gmail.com>
2023-09-04 11:01:16 +0200

    feat: Imagen de fondo.
    Añado imagen de fondo provisional para el desarrollo del proyecto. La idea es que el usuario añada y seleccione la imagen de fondo que desee.

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+/**
+* TODO: 
+* He puesto de forma general una imagen de fondo.
+* La propuesta inicial es que el usuario pueda establecer 
+* una imagen de fondo a su gusto, ya sea, subirla a la aplicación y establecer 
+* dicha imagen o de un grupo de imágenes que haya subido previamente.
+*/
+
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'anDesktopImage': "url('src/assets/img/bg-desktop.jpg')"
+      }
+    },
   },
   plugins: [require('@tailwindcss/aspect-ratio')
 ,require('@tailwindcss/forms')

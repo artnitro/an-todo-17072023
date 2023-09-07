8f976dd08adc73ae9b27375addadfacd1fd7c1cf
artnitro <qp.artnitro@gmail.com>
2023-09-07 10:26:00 +0200

    feat: Configurar componente como Standalone Components.
    Configuro el componente como Standalone Componente.

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'an-todo-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })

97990a7e4b31a342fed23c855936a2e54c2a8679
artnitro <qp.artnitro@gmail.com>
2023-10-30 12:42:30 +0100

    feat: Agregar interfaces para tipos de variables globales.
    Agrego interfaces para variables globales del fichero de configuración de la aplicación.

---
+++ public/src/shared/interfaces/config.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * Interfaces para variables globales de configuración.
+ */
+
+
+export interface Icolors {
+  [key: string]: any
+}
+
+export interface Iservices {
+  [key: string]: string
+}

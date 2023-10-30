e996f7c63d5aacfe737a10c284dbc87a6c3a127d
artnitro <qp.artnitro@gmail.com>
2023-10-30 12:53:18 +0100

    feat: Agregar nueva variable global.
    Agrego variable global SERVICES para la configuración de Apollo Client.

--- public/src/shared/config.ts
+++ public/src/shared/config.ts
@@ -2,10 +2,23 @@
  * Variables globales de configuración de la aplicación.
  */
 
+
+import { Icolors, Iservices } from "./interfaces/config.interface";
+
 /**
  * Colores.
  */
 
-export const COLORS: {[key: string]: any} = {
+export const COLORS: Icolors = {
   red: 'rgb(225 29 72)'
 }
+
+/**
+ * Servicios.
+ */
+
+export const SERVICES: Iservices = {
+  api: 'https://api.antodo.local:3000/api/v1/',
+  oauth: 'https://oauth.antodo.local:5000/oauth/v1/'
+}
+

b9f75c65b1cac43b86676c4be7549df00efcbe94
artnitro <qp.artnitro@gmail.com>
2023-10-30 12:58:50 +0100

    feat: Configurar Apollo Angular en la aplicación.
    Configuro Apollo Angular en la aplicación para las dos servicios que se van a ejecutar por ahora.

--- public/src/main.ts
+++ public/src/main.ts
@@ -1,10 +1,17 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { APOLLO_NAMED_OPTIONS, ApolloModule, NamedOptions } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
 
 import { environment } from './environments/environment.development';
 import { AppComponent } from './app/app.component';
 import { AppRoutes } from './app/app.routes';
+import { SERVICES } from './shared/config'; 
+
 
 /**
  * Comprobamos si se está ejecutando en modo PRODUCCIÓN.
@@ -20,7 +27,29 @@ import { AppRoutes } from './app/app.routes';
   try {
     await bootstrapApplication(AppComponent, {
       providers: [
+        importProvidersFrom(HttpClientModule, ApolloModule),
         provideRouter(AppRoutes),
+        {
+          provide: APOLLO_NAMED_OPTIONS,
+          useFactory(httpLink: HttpLink): NamedOptions {
+            return {
+              // default: { Con este valor, toma esta dirección por defecto. }
+              api: {
+                cache: new InMemoryCache(),
+                link: httpLink.create({
+                  uri: SERVICES['api'],
+                }),
+              },
+              oauth: {
+                cache: new InMemoryCache(),
+                link: httpLink.create({
+                  uri: SERVICES['oauth'],
+                }),
+              },
+            };
+          },
+          deps: [HttpLink],
+        }, 
       ]
     });
     console.info('an-INFO: Comienza la aplicacion AN-TODO-17072023.');

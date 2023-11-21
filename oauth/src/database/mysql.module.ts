a58234f8d47b5eab01b844685566b843b17c7500
artnitro <qp.artnitro@gmail.com>
2023-11-21 18:29:08 +0100

    feat: Configurar MySQL.
    Configuro conexión, database, usuario y demás característica de MySQL.

---
+++ oauth/src/database/mysql.module.ts
@@ -0,0 +1,48 @@
+/**
+ * Módulo de configuración de MySQL.
+ */
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+import { DataSource } from 'typeorm';
+
+
+//NOTE: synchronize: true, tenedlo sólo hasta que defina en concreto la base de datos con sus 
+// tablas correspondientes, ya que el esquema se autocrea cada vez que se lanza la aplicación.
+// NO USAR EN PRODUCCIÓN, ya que borra los datos. Usadlo para depuración y desarrollo.
+// Cuando esté todo definido, asignarlo a false.
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('HOST_MYSQL'),
+        port: configService.get('PORT_MYSQL'),
+        username: configService.get('USER_MYSQL'),
+        password: configService.get('PWD_MYSQL'),
+        database: configService.get('DB_MYSQL'),
+        poolSize: 5,
+        logging: ['query', 'error'],
+        entities: [
+          __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      }),
+      dataSourceFactory: async (options) => {
+        try {
+          const dataSource = await new DataSource(options).initialize();
+          console.info('AN-INFO: MySQL está configurada correctamente.');
+          return dataSource;
+        } catch (error) {
+          console.error('AN-ERROR: MySQL error en la configuración: ', error);
+        }
+      },
+    }),
+  ]
+})
+export class MysqlModule {}

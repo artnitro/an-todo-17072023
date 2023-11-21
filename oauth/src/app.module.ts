ac6b061dbb552da1c81d7f8a54b53bc48d31da32
artnitro <qp.artnitro@gmail.com>
2023-11-21 18:26:34 +0100

    feat: Configurar arranque de la aplicación.
    Configuro arranque de la aplicación respecto a módulos y controladores.

--- oauth/src/app.module.ts
+++ oauth/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MysqlModule } from './database/mysql.module';
+import { UserModule } from './database/user/user.module';
+//import { OauthModule } from './oauth/oauth.module';
+ 
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MysqlModule,
+    UserModule,
+  //  OauthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

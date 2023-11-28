8d14e02b491f9ac298ad61df9a8edc80860d7309
artnitro <qp.artnitro@gmail.com>
2023-11-28 19:27:22 +0100

    feat: Configurar GraphQL.
    Módulo de configuración de GRaphQL.

---
+++ oauth/src/database/graph.module.ts
@@ -0,0 +1,28 @@
+/**
+ * Módulo de configuración de GraphQL.
+ */
+
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { UserModule } from './user/user.module';
+
+
+@Module({
+  imports: [
+    UserModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      path: 'oauth/v1/',
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), 
+      sortSchema: true,
+      playground: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class GraphModule {}

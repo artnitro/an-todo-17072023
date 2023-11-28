b8585180f92efabb50e93a9fdf41d36acf3f644f
artnitro <qp.artnitro@gmail.com>
2023-11-28 19:20:35 +0100

    refactor: Gestionar errores en el arranque.
    Gestiono posibles errores en el arranque de la aplicación.

--- oauth/src/main.ts
+++ oauth/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  try {
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
+  console.info('AN-INFO: Arranque correcto de la aplicación.');
+  } catch (error) { 
+    console.error('AN-ERROR: Error en arranque de la aplicación. ', error);
+  }
 }
 bootstrap();

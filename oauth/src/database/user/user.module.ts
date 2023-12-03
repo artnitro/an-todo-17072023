b48347c8d7ba7be64805cff4b072a04842473c44
artnitro <qp.artnitro@gmail.com>
2023-12-03 11:51:26 +0100

    feat: Agregar funcionalidad para módulo JWT.
    Agrego y configuro funcionalidad para el módulo JWT.

--- oauth/src/database/user/user.module.ts
+++ oauth/src/database/user/user.module.ts
@@ -4,12 +4,26 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+import { UserService } from './user.service';
+import { UserResolver } from './user.resolver';
+
 import { User } from './user.entity';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+      }),
+    })
+  ],
+  providers: [UserService, UserResolver],
 })
 export class UserModule {}

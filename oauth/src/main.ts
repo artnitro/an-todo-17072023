26ebc6a496ac28449fca7db84eb276399cac0c91
artnitro <qp.artnitro@gmail.com>
2023-12-18 19:18:23 +0100

    feat: Agregar cookie-parter a la aplicación.
    Agrego y configuro  cookie-parser para la gestión de cookies en la aplicación.

--- oauth/src/main.ts
+++ oauth/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
+
 
 async function bootstrap() {
   try {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.use(cookieParser(configService.get('COOKIE_PARSER')));
   await app.listen(5000);
   console.info('AN-INFO: Arranque correcto de la aplicación.');
   } catch (error) { 

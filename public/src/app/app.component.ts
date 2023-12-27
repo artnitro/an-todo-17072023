2fa8dfbbd60d55e92a57902f6c7aecbdf8c97736
artnitro <qp.artnitro@gmail.com>
2023-12-27 12:17:48 +0100

    fix: Modificar configuración imagen de fondo.
    Modifico la configuración de la imagen de fondo, ya que presentaga error al modificar y actualiazar el tamaño de la ventana.

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgStyle } from '@angular/common';
 
 import { BGIMAGES } from 'src/shared/config';
 
@@ -8,15 +7,26 @@ import { BGIMAGES } from 'src/shared/config';
 @Component({
   selector: 'an-todo-root',
   standalone: true,
-  imports: [RouterOutlet, NgStyle],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  // Establezco imagen de fondo (provisional) de la aplicación.
+  constructor(
+    private renderer: Renderer2,
+  ){}
 
-  bgImage: string = BGIMAGES['global'];
+  ngOnInit() {
+
+    const bgImage: string = BGIMAGES['global'];
+
+    // Establezco imagen de fondo (provisional) de la aplicación.
+
+    this.renderer.setStyle(document.body, 'background', 'url(' + bgImage + ') no-repeat center center fixed');
+	  this.renderer.setStyle(document.body, 'background-size', 'cover');
+
+  }
   
 }
 

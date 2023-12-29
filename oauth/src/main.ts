a0f52d556afd4fd8e3168cf9e18aae6a8d0930f5
artnitro <qp.artnitro@gmail.com>
2023-12-29 11:11:45 +0100

    feat: Modificar política CORS.
    La gestión de la política de CORS la gestiono a traves de NestJS, elminando el control de dicha política de Nginx.

--- oauth/src/main.ts
+++ oauth/src/main.ts
@@ -8,15 +8,25 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
+  
   try {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  app.use(cookieParser(configService.get('COOKIE_PARSER')));
-  app.useGlobalPipes(new ValidationPipe( { transform: true }));
-  await app.listen(5000);
-  console.info('AN-INFO: Arranque correcto de la aplicación.');
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+  
+    app.enableCors({
+      'origin': configService.get('CORS_ORIGIN'),
+      'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      'allowedHeaders': 'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With',
+      'credentials': true,
+    });
+    app.use(cookieParser(configService.get('COOKIE_PARSER')));
+    app.useGlobalPipes(new ValidationPipe( { transform: true }));
+    await app.listen(5000);
+    console.info('AN-INFO: Arranque correcto de la aplicación.');
   } catch (error) { 
     console.error('AN-ERROR: Error en arranque de la aplicación. ', error);
   }
+
 }
+
 bootstrap();

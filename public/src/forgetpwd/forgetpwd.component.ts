7747bae70e2d4322d56ddcdfa9665f0f86e7fcc1
artnitro <qp.artnitro@gmail.com>
2024-01-23 18:51:34 +0100

    feat: Comenzar opción "olvidastes tu contraseña".
    Comienzo opción "olvidastes tu contraseña".

---
+++ public/src/forgetpwd/forgetpwd.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'an-todo-forgetpwd',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './forgetpwd.component.html',
+  styleUrls: ['./forgetpwd.component.scss']
+})
+export class ForgetpwdComponent {
+
+}

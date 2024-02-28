211224402b9cf4fb10a9ed76989cfa167d0b84c8
artnitro <qp.artnitro@gmail.com>
2024-02-28 18:15:06 +0100

    feat: Agregar funcionalidad a Signup Component
    Agrego toda la funcionalidad al Componente Signup.

--- public/src/signup/signup.component.ts
+++ public/src/signup/signup.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { NgStyle } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+
+import { Apollo } from 'apollo-angular';
+
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { FormFieldsAbstract } from 'src/shared/Forms/form-fields.abstract';
 import { CardSessionComponent } from 'src/components/card-session/card-session.component'; 
 import { FormService } from 'src/services/forms/form.service';
 import { COLORS } from 'src/shared/config';
 import { ConfirmPasswordValidator } from 'src/shared/Forms/validators/confirm-password.validator';
+import { SignupService } from './signup.service';
+import { user } from 'src/shared/signals/user.signal';
+import { Unsubscribe } from 'src/decorators/unsubscribe.decorator';
 
 @Component({
   selector: 'an-todo-signup',
@@ -21,9 +29,11 @@ import { ConfirmPasswordValidator } from 'src/shared/Forms/validators/confirm-pa
   templateUrl: 'signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
+@Unsubscribe()
 export class SignupComponent extends FormFieldsAbstract implements OnInit {
 
   signupForm!: FormGroup;
+  subscription$: Subscription = new Subscription();
 
   title: string = 'Regístrate';
   hasError: {[key: string]: any} = {};
@@ -32,6 +42,9 @@ export class SignupComponent extends FormFieldsAbstract implements OnInit {
   constructor(
     private fb:FormBuilder,
     private formService:FormService,
+    private apollo: Apollo,
+    private signupService: SignupService,
+    private router: Router,
   ) {
     super();
   }
@@ -57,7 +70,36 @@ export class SignupComponent extends FormFieldsAbstract implements OnInit {
 
     this.hasError = this.formService.hasFieldError(this.signupForm);
 
-    console.log('an-LOG: Elementos del formulario con error: ', this.hasError);
+    if ( Object.keys(this.hasError).length === 0 ) {
+      this.subscription$ = this.signupService
+        .createUser$({
+          firstName: this.signupForm.value.firstName,
+          lastName: this.signupForm.value.lastName,
+          email: this.signupForm.value.email,
+          nickName: this.signupForm.value.nickName,
+          password: this.signupForm.value.password
+        })
+        .pipe(map(result => result.data.createUser))
+        .subscribe({
+          next: (createUser) => {
+            user.set(createUser.access_token);
+            this.router.navigate(['/dashboard'])
+          },
+          error: (err) => {
+            Object
+              .keys(err.graphQLErrors)
+              .filter(element => {
+                const { originalError } = err.graphQLErrors[element];
+                if ( originalError.statusCode === 400 || originalError.statusCode === 401 || originalError.statusCode === 406) {
+                  Object
+                    .keys(this.signupForm.controls)
+                    .filter(value => this.signupForm.controls[value].setValue(''));
+                  this.hasError = this.formService.hasFormError(this.signupForm);
+                }
+              });
+          }
+        });
+    }
 
   }
 

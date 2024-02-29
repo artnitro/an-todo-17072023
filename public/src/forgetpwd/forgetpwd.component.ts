6f6f2c73d8baac5ee48b22bfc287cbbaaf69c9fd
artnitro <qp.artnitro@gmail.com>
2024-02-29 19:01:16 +0100

    feat: Agregar funcionalidad para mostrar campos del formulario.
    Agrego funcionalidad para mostrar u ocultar campo de email o texto explicativo para indicar lo que tiene que hacer.

--- public/src/forgetpwd/forgetpwd.component.ts
+++ public/src/forgetpwd/forgetpwd.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+/**
+ * Componente Forget Passwword.
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterLink, Router} from '@angular/router';
+
+import { FormFieldsAbstract } from 'src/shared/Forms/form-fields.abstract';
+import { CardSessionComponent } from 'src/components/card-session/card-session.component';
+import { COLORS } from 'src/shared/config';
 
 
 @Component({
   selector: 'an-todo-forgetpwd',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    NgStyle,
+    RouterLink,
+    ReactiveFormsModule,
+    CardSessionComponent,
+  ],
   templateUrl: './forgetpwd.component.html',
   styleUrls: ['./forgetpwd.component.scss']
 })
-export class ForgetpwdComponent {
+export class ForgetpwdComponent extends FormFieldsAbstract implements OnInit {
+
+  forgetForm!: FormGroup;
+
+  title: string = '¿Olvidaste la contraseña?';
+  hasError: {[key: string]: any} = {};
+  colors: {[key: string]: any} = COLORS;
+  showPanel: boolean = true;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    super();
+  }
+  ngOnInit(): void {
+
+    console.info('an-INNFO: Ejecutando ForgetpwdComponent.');
+
+    this.forgetForm = this.fb.group({
+      email: this.email(),
+    })
+
+  }
+
+  checkForm(): void {
+
+  }
 
 }

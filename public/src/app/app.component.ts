8cd104558dd060c7160b5f3f95e55a67fb3bef3e
artnitro <qp.artnitro@gmail.com>
2024-03-07 18:33:07 +0100

    feat: Gestionar la lectura de parámetros en la URL
    Gestiono la lectura del parámetro de la URL para poder cambiar el password de usuario.

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component, Renderer2, OnInit } from '@angular/core';
+import { Component, Renderer2, OnInit, Inject } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { BGIMAGES } from 'src/shared/config';
 import { AppService } from './app.service';
-import { user } from 'src/shared/signals/user.signal';
+import { user, userForgetpwd } from 'src/shared/signals/user.signal';
 import { Unsubscribe } from 'src/decorators/unsubscribe.decorator';
 
 
@@ -20,19 +21,20 @@ import { Unsubscribe } from 'src/decorators/unsubscribe.decorator';
 @Unsubscribe()
 export class AppComponent implements OnInit {
 
-  querySubscription!: Subscription;
   querySubscription$: Subscription = new Subscription();
 
   constructor(
     private renderer: Renderer2,
     private appService: AppService,
     private router: Router,
+    @Inject(DOCUMENT) private document: Document, 
   ){}
 
   ngOnInit() {
 
     const bgImage: string = BGIMAGES['global'];
-
+    const userForgetPasswordData: any = this.document.defaultView?.location.href.split('=')[1];
+    
     // Establezco imagen de fondo (provisional) de la aplicación.
 
     this.renderer.setStyle(document.body, 'background', 'url(' + bgImage + ') no-repeat center center fixed');
@@ -40,25 +42,30 @@ export class AppComponent implements OnInit {
 
     // Gestiono datos de usuario.
 
-    this.querySubscription$ = this.appService
-      .refreshUser$()
-      .pipe(map(result => result.data.refreshUser))
-      .subscribe({
-        next: (refreshUser) => {
-          user.set(refreshUser.access_token);
-          this.router.navigate(['/dashboard']);
-        },
-        error: (err) => {
-          Object
-            .keys(err.graphQLErrors)
-            .filter( element => {
-              const { originalError } = err.graphQLErrors[element];
-              if ( originalError.statusCode === 400 || originalError.statusCode === 401 ) {
-                this.router.navigate(['/signin']);
-              }
-            });
-        }
-      });
+    if ( this.document.defaultView?.location.href.indexOf('user') !== -1) {
+      userForgetpwd.set(userForgetPasswordData);
+      this.router.navigate(['/forgetpwd']);
+    } else {
+      this.querySubscription$ = this.appService
+        .refreshUser$()
+        .pipe(map(result => result.data.refreshUser))
+        .subscribe({
+          next: (refreshUser) => {
+            user.set(refreshUser.access_token);
+            this.router.navigate(['/dashboard']);
+          },
+          error: (err) => {
+            Object
+              .keys(err.graphQLErrors)
+              .filter( element => {
+                const { originalError } = err.graphQLErrors[element];
+                if ( originalError.statusCode === 400 || originalError.statusCode === 401 ) {
+                  this.router.navigate(['/signin']);
+                }
+              });
+          }
+        });
+    }
 
   }
 

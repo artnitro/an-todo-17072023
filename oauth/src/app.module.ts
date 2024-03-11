30d80a0e6ea1755ec6a51e3a8dd90ac3389d38d7
artnitro <qp.artnitro@gmail.com>
2024-03-11 17:46:21 +0100

    feat: Configurar Redis con NestJS.
    configuro Redis con NestJS para el trabajo de cache.

--- oauth/src/app.module.ts
+++ oauth/src/app.module.ts
@@ -4,9 +4,11 @@
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 import { MysqlModule } from './database/mysql.module';
 import { GraphModule } from './database/graph.module';
+import { RedisConfig } from './database/redis.config';
  
 
 @Module({
@@ -14,6 +16,7 @@ import { GraphModule } from './database/graph.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    CacheModule.registerAsync(RedisConfig),
     MysqlModule,
     GraphModule,
   ],

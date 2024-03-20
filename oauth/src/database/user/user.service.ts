5986ffad9e7011e5f547ec2798c4dc1712fd9cdb
artnitro <qp.artnitro@gmail.com>
2024-03-20 17:40:46 +0100

    feat: Agregar gestión de cache de usuario y actualizar password
    Agrego gestion de cache de usuario para pode modificar el password y la gestión de la actualización del password

--- oauth/src/database/user/user.service.ts
+++ oauth/src/database/user/user.service.ts
@@ -1,5 +1,5 @@
 /**
- * User service. Gestión CRUD del usuario.
+ * User service. Gestión CRUD y Cache del usuario.
  */
 
 import { Injectable, Inject} from '@nestjs/common';
@@ -7,13 +7,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { CONTEXT } from '@nestjs/graphql';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 import { Repository } from 'typeorm';
+import { Cache } from 'cache-manager';
+import { v4 as uuidv4 } from 'uuid';
 
 import { User } from './user.entity';
 import { UserInput } from './dto/user.input';
 import { Token } from './dto/token.args';
 import { UserData } from './dto/user-data.args';
+import { ChangepwdInput } from './dto/changepwd.input';
 
 
 @Injectable()
@@ -24,8 +28,8 @@ export class UserService {
     private userRepository: Repository<User>,
     private jwtService: JwtService,
     private configService: ConfigService,
-    @Inject(CONTEXT)
-    private context,
+    @Inject(CONTEXT) private context,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ){}
 
   /**
@@ -48,6 +52,24 @@ export class UserService {
     
     return await this.userRepository.findOneBy(data)
     
+  }
+
+  /**
+   * @description Actualiza el password del usuario
+   * @param changepwdInput 
+   * @returns Promise<User>
+   */
+  async updatePassword ( changepwdInput: ChangepwdInput ): Promise<User> {
+
+    let  
+      email,
+      password,
+      mail = ({ email } = changepwdInput, { email }),
+      pwd =  ({ password } = changepwdInput, { password });
+
+    await this.userRepository.update( mail, pwd );
+    return await this.userRepository.findOneBy(mail);
+
   }
 
   /**
@@ -87,6 +109,30 @@ export class UserService {
       sameSite: 'strict',
       signed: true,
     });
+
+  }
+
+  /**
+   * @description Establece en cache la clave del usuario y email
+   * @param data 
+   * @returns Promise<string>
+   */
+  async setForgetpwd(data: string): Promise<string> {
+    
+    const uuid: string = uuidv4();
+    await this.cacheManager.set(uuid, data);
+    return uuid;
+
+  }
+
+  /**
+   * @description Obtiene si el usuario esta aún en cache
+   * @param data 
+   * @returns Promise<string | null>
+   */
+  async getForgetpwd(data: string): Promise<string | null> {
+
+    return await this.cacheManager.get(data);
 
   }
 

f15a76b725617480f2ba11a61fb01a4688438820
artnitro <qp.artnitro@gmail.com>
2024-03-20 17:45:00 +0100

    feat: Agregar consultas para la gestión del password
    Agrego consultas para la gestión de modificación o cambio del password del usuario.

--- oauth/src/database/user/user.resolver.ts
+++ oauth/src/database/user/user.resolver.ts
@@ -2,6 +2,8 @@
  *  Resolver para User
  */
 
+//TODO: Configurar el envío del correo para el cambio o actualización del password.
+
 import { UnauthorizedException, NotAcceptableException, HttpException , Inject} from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, CONTEXT } from '@nestjs/graphql';
 import { ConfigService } from '@nestjs/config';
@@ -13,6 +15,9 @@ import { User } from './user.entity';
 import { UserInput } from './dto/user.input';
 import { Token } from './dto/token.args';
 import { IsUser } from './dto/is-user.args';
+import { UserForgetpwd } from './dto/user-forgetpwd.args';
+import { IsuserForgetpwd } from './dto/isuser-fogetpwd.args';
+import { ChangepwdInput } from './dto/changepwd.input';
 
 
 @Resolver( of => User)
@@ -63,7 +68,30 @@ export class UserResolver {
     }
       
   }
-  
+
+  @Query( returns => String, { description: 'Identidicación del usuario que olvida o quiere cambiar el password'})
+  async uuidForgetpwd(@Args() userForgetpwd: UserForgetpwd): Promise<string> {
+
+    let  
+      email,
+      mail = ({ email } = userForgetpwd, { email });
+    
+    const user = await this.userService.findOne(mail);
+
+    if ( user !== null ) { 
+      return await this.userService.setForgetpwd(user.email);
+    } else {
+      throw new UnauthorizedException('Usuario no autorizado', { cause: new Error(), description: 'Usuario no autorizado o error en credenciales'});
+    }
+
+  }
+
+  @Query(() => String, { description: 'Comprueba si el usuario permanece aún en la cache del sistema para el cambio de contraseña', nullable: true})
+  async isUserForguetpwd(@Args() isuserForgetpwd: IsuserForgetpwd): Promise<string>  {
+
+    return await this.userService.getForgetpwd(isuserForgetpwd.uuid);
+    
+  }
 
   @Mutation( returns => Token, { description: 'Consulta para crear un usuario' })
   async createUser(
@@ -85,6 +113,23 @@ export class UserResolver {
       throw new NotAcceptableException('Usuario ya registrado', { cause: new Error(), description: 'Usuario ya está regitrado en el sistema'});
     }
     
+  }
+
+  @Mutation ( returns => Boolean , { description: 'Consulta para cambiar el password' })
+  async changePassword(
+    @Args('changepwdInput') changepwdInput: ChangepwdInput,
+  ): Promise<boolean> {
+
+    const salt = bcrypt.genSaltSync(parseInt(this.configService.get('SALT_BCRYPT'),10));
+
+    changepwdInput.password = bcrypt.hashSync(changepwdInput.password, salt);
+
+    if ( await this.userService.updatePassword(changepwdInput) !== null ) {
+      return true
+    } else {
+      throw new UnauthorizedException('Usuario no autorizado', { cause: new Error(), description: 'Usuario no autorizado o error en credenciales'});
+    }
+
   }
 
 }

056dc6ca271318302c8d79e2c78495d2eddd1867
artnitro <qp.artnitro@gmail.com>
2024-05-17 18:21:50 +0200

    feat: Agregar funcionalidad para CardPasswordComponet
    Agrego la funcionalidad del componente CardPasswordComponent, para la gestión de la información del usuario referente a la modificación de la contraseña.

--- public/src/forgetpwd/forgetpwd.component.ts
+++ public/src/forgetpwd/forgetpwd.component.ts
@@ -18,7 +18,7 @@ import { userForgetpwd } from 'src/shared/signals/user.signal';
 import { FormService } from 'src/services/forms/form.service';
 import { Unsubscribe } from 'src/decorators/unsubscribe.decorator';
 import { ForgetpwdService } from './forgetpwd.service';
-
+import { CardPasswordComponent } from 'src/components/card-password/card-password.component';
 
 @Component({
   selector: 'an-todo-forgetpwd',
@@ -28,6 +28,7 @@ import { ForgetpwdService } from './forgetpwd.service';
     RouterLink,
     ReactiveFormsModule,
     CardSessionComponent,
+    CardPasswordComponent,
   ],
   templateUrl: './forgetpwd.component.html',
   styleUrls: ['./forgetpwd.component.scss']
@@ -38,23 +39,21 @@ export class ForgetpwdComponent extends FormFieldsAbstract implements OnInit {
   forgetEmailForm!: FormGroup;
   forgetPasswordForm!: FormGroup;
   subscription$: Subscription = new Subscription();
-
   title: string = '¿Olvidaste la contraseña?';
   hasEmailError: {[key: string]: any} = {};
   hasPasswordError: {[key: string]: any} = {};
   colors: {[key: string]: any} = COLORS;
   userEmail: string = '';
   showEmail: boolean = true;
-  showPanel: boolean = false;
   showPassword: boolean = false;
-  showPanelPwd: boolean = false;
-  showSend: Boolean = true;
+  showSend: boolean = true;
+  panelInfo:boolean = false;
+  textInfo: string = ''; 
 
   constructor(
     private fb: FormBuilder,
     private formService: FormService,
     private forgetpwdService: ForgetpwdService,
-    private router: Router,
   ) {
     
     super();
@@ -74,16 +73,17 @@ export class ForgetpwdComponent extends FormFieldsAbstract implements OnInit {
                 this.showEmail = false;
                 this.showPassword = true;
                 this.showSend = true;
+                userForgetpwd.set('ok');
               } else {
                 this.showEmail = false;
-                this.showPanelPwd = true;
+                this.panelInfo = true;
+                this.textInfo = 'El identificador de usuario ha expirado o el identificador de usuario es incorrecto.'
                 this.showSend = false;
+                userForgetpwd.set('ok');
               }
             }
           })
         );
-      } else {
-        this.showEmail = true;
       }
       
     });
@@ -124,9 +124,9 @@ export class ForgetpwdComponent extends FormFieldsAbstract implements OnInit {
               //console.log('an-LOG: uuid de usuario: ', uuidForgetpwd);
 
               this.showEmail = false;
-              this.showPanel = true;
+              this.panelInfo = true;
+              this.textInfo = 'Recibirás un mensaje a esa dirección de correo para poder modificar tu contraseña.' 
               this.showPassword = false;
-              this.showPanelPwd = false;
               this.showSend = false;
             },
             error: (err) => {
@@ -134,11 +134,16 @@ export class ForgetpwdComponent extends FormFieldsAbstract implements OnInit {
                 .keys(err.graphQLErrors)
                 .filter( element => {
                   const { originalError } = err.graphQLErrors[element];
-                  if ( originalError.statusCode === 400 || originalError.statusCode === 401 ) {
+                  if ( originalError.statusCode === 401 || Array.isArray(originalError['message']) ) {
                     Object
                       .keys(this.forgetEmailForm.controls)
                       .filter(value => this.forgetEmailForm.controls[value].setValue(''));
                     this.hasEmailError = this.formService.hasFormError(this.forgetEmailForm);
+                  } else {
+                    this.panelInfo = true;
+                    this.textInfo = 'Fallo en el servidor de envío de correo electrónico. Intente más tarde la solicitud de autorización para el cambio de contraseña. Disculpe las molestias.';
+                    this.showEmail = false;
+                    this.showSend = false;
                   }
                 });
             } 
@@ -161,10 +166,12 @@ export class ForgetpwdComponent extends FormFieldsAbstract implements OnInit {
           .pipe(map(result => result.data.changePassword))
           .subscribe({
             next: (changePassword) => {
-              
-              //(console.log('an-LOG: Estado del cambio de password: ', changePassword);
-              
-              this.router.navigate(['/signin']);
+              this.showEmail = false;
+              this.showPassword = false;
+              this.showSend = false;
+              this.panelInfo = true;
+              this.textInfo = 'Su contraseña se ha modificado correctamente. Con esta nueva contraseña podrá acceder a la aplicación.';
+              userForgetpwd.set('ok');   
             },
             error: (err) => {
               Object

95bfbf97c41ce910b9aaf01c31cfa05abe67454e
artnitro <qp.artnitro@gmail.com>
2024-05-24 18:03:51 +0200

    feat: Configurar GraphQL para consultas
    Configuro GraphQL para consultas con el servidor de tareas de usuario, además de conexiones socket.

--- public/src/shared/graphql/config.graphql.ts
+++ public/src/shared/graphql/config.graphql.ts
@@ -2,20 +2,35 @@
  * Configuración de GraphQL.
  */
 
-// NOTE: Eliminar console para producción. La dejo en desarollo para tener una mayor información de
+// NOTE: Eliminar console de los errores y demás para producción. Lo dejo en desarollo para tener una mayor información de
 // de los errores producidos.
 // NOTE: En la configuración de onError, está el parámetro response. Puedo jugar con él 
 // igual que graphQLErrors y networkError. Si hiciese falta, atacarlo.
 
+import { effect } from '@angular/core';
 import { ApolloLink, InMemoryCache } from '@apollo/client/core';
 import { onError } from '@apollo/client/link/error';
 import { NamedOptions } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+
 import { environment } from '../../environments/environment.development';
+import { user } from '../signals/user.signal';
 
 
-export function OauthNamedGraphql( httpLink: HttpLink ): NamedOptions {
+export function ConfigGraphql( httpLink: HttpLink ): NamedOptions {
+
+  let token: string = '';
+  
+  effect( () => {
+
+      token = user();
+      console.log('an-LOG: El actual valor de user-signal:', token);
+
+  });
 
   // Configuración error.
 
@@ -34,21 +49,58 @@ export function OauthNamedGraphql( httpLink: HttpLink ): NamedOptions {
 
   });
 
+  // Configuración authoritation header.
+
+  const authLink = new ApolloLink((operation, forward) => {
+    operation.setContext({
+      headers: {
+        'Authorization': token ? `Bearer ${token}` : '',
+      }
+    });
+    return forward(operation);
+  });
+
   // Configuración http link.
 
-  const http = httpLink.create({
+  const httpOauth = httpLink.create({
     uri: environment.oauth,
     withCredentials: true,
   });
 
+  const httpTask = httpLink.create({
+    uri: environment.task,
+    withCredentials: true,
+  });
+
+  // Configuración socket link.
+
+  const wss = new GraphQLWsLink(createClient({
+    url: environment.taskWss,
+    connectionParams: {
+      Authorization: token,
+    }
+  }));
+
   // Configuración link.
 
-  const link = ApolloLink.from([errorLink, http]);
+  const linkOauth = ApolloLink.from([errorLink, httpOauth]);
+
+  const linkTask = ApolloLink.split( ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+    wss,
+    ApolloLink.from([errorLink, authLink, httpTask]),
+  );
 
   return {
     oauth: {
       cache: new InMemoryCache(),
-      link: link,
+      link: linkOauth,
+    }, 
+    task: {
+      cache: new InMemoryCache(),
+      link: linkTask,
     }
   }
   

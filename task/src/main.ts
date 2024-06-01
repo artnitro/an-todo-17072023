1bf5fa619f9500a2e1138915bf81ac0cae05db2e
artnitro <qp.artnitro@gmail.com>
2024-06-01 18:58:45 +0200

    feat: Configurar módulos de arranque
    Configuro ConfigService para acceso global, ValidationPipe para validaciones de campos y habilito CORS.

--- task/src/main.ts
+++ task/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe} from '@nestjs/common';
+
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  try {
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
+    app.enableCors({
+      'origin': configService.get('CORS_ORIGIN'),
+      'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      'allowedHeaders': 'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With',
+      'credentials': true,
+    });
+    app.use(cookieParser(configService.get('COOKIE_PARSER')));
+    app.useGlobalPipes(new ValidationPipe( { transform: true }));
+    await app.listen(3000);
+    console.info('an-INFO: Arranque correcto de la aplicación.');
+  } catch (error) {
+    console.error('an-ERROR: Error en arranque de la aplicación. ', error);
+  }
+
 }
+
 bootstrap();

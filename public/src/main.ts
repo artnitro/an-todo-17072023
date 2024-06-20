5eeb398c19669d06622b7794efb2087e8e548d3c
artnitro <qp.artnitro@gmail.com>
2024-06-20 18:36:13 +0200

    feat: Agregar módulos a la aplicación
    Agrego módulos oauth y task a la aplicación y anulo a los que suplantan de forma provisional.

--- public/src/main.ts
+++ public/src/main.ts
@@ -1,3 +1,7 @@
+/**
+ * Arranque de la aplicación an-todo-16072023.
+ */
+
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
@@ -9,8 +13,11 @@ import { HttpLink } from 'apollo-angular/http';
 import { environment } from './environments/environment.development';
 import { AppComponent } from './app/app.component';
 import { AppRoutes } from './app/app.routes';
-import { ConfigGraphql } from './shared/graphql/config.graphql';
+//import { ConfigGraphql } from './shared/graphql/config.graphql';
 //import { ApiGraphql } from './shared/graphql/api.graphql';
+
+import { OauthModule } from './shared/graphql/oauth.module';
+import { TaskModule } from './shared/graphql/task.module';
 
 
 //Comprobamos si se está ejecutando en modo PRODUCCIÓN.
@@ -23,18 +30,23 @@ import { ConfigGraphql } from './shared/graphql/config.graphql';
   try {
     await bootstrapApplication(AppComponent, {
       providers: [
-        importProvidersFrom(HttpClientModule, ApolloModule),
+        importProvidersFrom(
+          HttpClientModule, 
+          ApolloModule, 
+          TaskModule, 
+          OauthModule,
+        ),
         provideRouter(AppRoutes, withComponentInputBinding()),
         /* {
           provide: APOLLO_OPTIONS,
           useFactory: ApiGraphql,
           deps: [HttpLink],
         }, */
-        {
+        /* {
           provide: APOLLO_NAMED_OPTIONS,
           useFactory: ConfigGraphql, 
           deps: [HttpLink],
-        },
+        }, */
       ]
     });
     console.info('an-INFO: Comienza la aplicacion AN-TODO-17072023.');

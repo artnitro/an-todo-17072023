9abdbf5e2a3701b6a71c764fb3a0af628038941b
artnitro <qp.artnitro@gmail.com>
2024-06-24 18:11:06 +0200

    feat: Agregar módulo de configuración de GraphQL
    Agrego el módulo de configuración de GraphQL.

---
+++ task/src/config/graphql.module.ts
@@ -0,0 +1,48 @@
+/**
+ * Módulo de configuración de GraphQL.
+ */
+
+// NOTE: Si es necesario para obtener mayor información de los errores producidos en 
+// desarrollo, comentar el método formatError de GraphQLModule y descomentar cuando 
+// no sea necesario. Puende ocasionar errores en en frontend, sdebido a que se espera
+// una respuesta formateada para los errores así.
+// NOTE: La propiedad message, de formatError, siempre tiene que retornarse ya que 
+// causa error sino se retorna.
+
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+import { UserModule } from 'src/user/user.module';
+
+
+@Module({
+  imports: [
+    UserModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      path: 'task/v1/',
+      context: ({ req, res }) => ({ req, res }),
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), 
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      formatError: ({ message, extensions }) => {
+        const originalError = extensions.originalError;
+        return {
+          message,
+          originalError,
+        }
+      },
+      subscriptions: {
+        'graphql-ws': true,
+      },
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class GraphqlModule {}

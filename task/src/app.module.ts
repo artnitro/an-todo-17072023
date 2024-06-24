49ca84ab754525a20ce16619070e3ec012f22906
artnitro <qp.artnitro@gmail.com>
2024-06-24 18:13:56 +0200

    feat: Agregar el modulo de GrapHQL a la aplicación
    Agrego el módulo de GraphQL a la aplicación.

--- task/src/app.module.ts
+++ task/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { MongodbModule } from './config/mongodb.module';
+import { GraphqlModule } from './config/graphql.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,8 +13,9 @@ import { AppService } from './app.service';
       isGlobal: true,
     }),
     MongodbModule,
+    GraphqlModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}

fc81a108f19f6e91b029a45c039a1dac479ca2c7
artnitro <qp.artnitro@gmail.com>
2024-07-09 18:43:30 +0200

    feat: Agregar módulo PUB_SUB
    Agrego módulo de configuración PUB_SUB para graphql-redis-subscriptions.

---
+++ task/src/pubsub/pubsub.module.ts
@@ -0,0 +1,31 @@
+/**
+ * Módulo pubsub para websockets.
+ */
+
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+
+
+export const PUB_SUB = 'PUB_SUB'
+
+@Module({
+  providers: [
+    {
+      provide: PUB_SUB,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => 
+        new RedisPubSub ({
+          connection: {
+            host: configService.get('HOST_REDIS'),
+            port: configService.get('PORT_REDIS'),
+            password: configService.get('PWD_REDIS'),
+            db: 1,
+          }
+        })
+    },
+  ],
+  exports: [PUB_SUB]
+})
+export class PubsubModule {}

bc5a9520e46af875ab72f6767356c07d1874cd5a
artnitro <qp.artnitro@gmail.com>
2024-08-08 09:46:00 +0200

    feat: Eliminar provider
    Eloimino el provider AppProvider de la aplicación.

--- task/src/app.module.ts
+++ task/src/app.module.ts
@@ -3,12 +3,11 @@
  */
 
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { MongodbModule } from './config/mongodb.module';
 import { GraphqlModule } from './config/graphql.module';
 import { PubsubModule } from './pubsub/pubsub.module';
-import { AppResolver } from './app.resolver';
 
 
 @Module({
@@ -16,10 +15,18 @@ import { AppResolver } from './app.resolver';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    /* JwtModule.registerAsync({ // Dejar esto por ahora, puede que haga falta para auth guard de query y mutation. Si no fuese bien, inserto el ConfigService en el auth guard.
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+      }),
+    }), */
     MongodbModule,
     GraphqlModule,
     PubsubModule,
   ],
-  providers: [ AppResolver],
+  providers: [],
 })
 export class AppModule {}

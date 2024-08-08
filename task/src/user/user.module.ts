3ce858ff4062ba3397743a1b744b07e0ffb58fe3
artnitro <qp.artnitro@gmail.com>
2024-08-08 09:54:17 +0200

    feat: Agregar el servicio UserService
    Agrego el servicio UserService.

--- task/src/user/user.module.ts
+++ task/src/user/user.module.ts
@@ -6,12 +6,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
+import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ User ]),
   ],
-  providers: [ UserResolver ],
+  providers: [ UserService, UserResolver ],
 })
 export class UserModule {}

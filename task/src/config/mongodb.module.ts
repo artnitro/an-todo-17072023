8c13f1ed5ebd0d25d86bdce1cc24e567d399443f
artnitro <qp.artnitro@gmail.com>
2024-09-24 17:38:19 +0200

    feat: Crear el módulo de configuración de MongoDB
    Creo el módulo de configuración de MongoDB.

--- task/src/config/mongodb.module.ts
+++ task/src/config/mongodb.module.ts
@@ -3,44 +3,17 @@
  */
 
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { DataSource } from 'typeorm';
-
-
-// NOTE: synchronize: true, tenedlo sólo hasta que defina en concreto la base de datos con sus 
-// tablas correspondientes, ya que el esquema se autocrea cada vez que se lanza la aplicación.
-// NO USAR EN PRODUCCIÓN, ya que borra los datos. Usadlo para depuración y desarrollo.
-// Cuando esté todo definido, asignarlo a false.
+import { MongooseConfigService } from './mongoose-config.service';
 
 
 @Module({
-  imports: [ 
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mongodb',
-        host: configService.get('HOST_MONGO'),
-        port: configService.get('PORT_MONGO'),
-        username: configService.get('USER_MONGO'),
-        password: configService.get('PWD_MONGO'),
-        database: configService.get('DB_MONGO'),
-        maxPoolSize: 5,
-        logging: ['query', 'error'],
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-      dataSourceFactory: async (options) => {
-        try {
-          const dataSource = await new DataSource(options).initialize();
-          console.info('an-INFO: MongoDB está configurada correctamente.');
-          return dataSource;
-        } catch (error) {
-          console.error('an-ERROR: MongoDB error en la configuración: ', error);
-        }
-      },
-    }),
+  imports: [
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    })
   ]
 })
 export class MongodbModule {}

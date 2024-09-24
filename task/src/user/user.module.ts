0e5ab9bcebc700b61470a414520582cdc0059b17
artnitro <qp.artnitro@gmail.com>
2024-09-24 17:42:38 +0200

    feat: Definir collecciones en Mongoose
    Defino colecciones con las que voy a trabajar, además de los servicios y resolvers.

--- task/src/user/user.module.ts
+++ task/src/user/user.module.ts
@@ -3,16 +3,34 @@
  */
 
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { User } from './user.entity';
+import { User, UserSchema } from './user.entity';
+import { Project, ProjectSchema } from 'src/project/project.entity';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import { ProjectService } from 'src/project/project.service';
+import { ProjectResolver } from 'src/project/project.resolver';
+
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ User ]),
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+      {
+        name: Project.name,
+        schema: ProjectSchema,
+      }
+    ]),
   ],
-  providers: [ UserService, UserResolver ],
+  providers: [ 
+    UserService, 
+    UserResolver,
+    ProjectService,
+    ProjectResolver,
+  ],
 })
 export class UserModule {}

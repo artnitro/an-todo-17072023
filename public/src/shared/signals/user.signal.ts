3000dd3ef1efeb0f184cfc895b032180dda4043d
artnitro <qp.artnitro@gmail.com>
2024-11-14 18:35:23 +0100

    Eliminar ficheros
    Elimino ficheros sin utilidad.

--- public/src/shared/signals/user.signal.ts
+++
@@ -1,8 +0,0 @@
-/**
- * signal para el usuario.
- */
-
-import { WritableSignal, signal } from '@angular/core';
-
-export const user: WritableSignal<string> = signal('ok');
-export const userForgetpwd: WritableSignal<string> = signal('ok');

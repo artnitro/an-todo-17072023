73f916730d11a64ae8990fdb3bd5e8ed2f6e0edf
artnitro <qp.artnitro@gmail.com>
2025-09-06 12:23:29 +0200

    feat: Agregar mutation agregar usuario
    Agrego consulta SetUser para agregar usuario a MongoDB si el usuario no está registrado.

--- public/src/dashboard/dashboard.mutation.ts
+++ public/src/dashboard/dashboard.mutation.ts
@@ -3,3 +3,15 @@
  */
 
 import { gql } from 'apollo-angular';
+
+export const SET_USER = gql`
+  mutation SetUser($firstName: String!, $nickName: String, $email: String!) {
+    setUser(userInput: {
+      firstName: $firstName,
+      nickName: $nickName,
+      email: $email
+    }) {
+      _id
+    }
+  }
+`;
